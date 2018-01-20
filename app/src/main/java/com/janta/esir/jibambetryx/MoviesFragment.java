c765de5174ee4b8bd4ca7fbef59c33c806476964
esirK <esirkings@gmail.com>
2018-01-20 04:46:42 +0300

    Add a recycler view for displaying movie items on the home page

--- app/src/main/java/com/janta/esir/jibambetryx/MoviesFragment.java
+++ app/src/main/java/com/janta/esir/jibambetryx/MoviesFragment.java
@@ -3,24 +3,60 @@ package com.janta.esir.jibambetryx;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+
+import com.janta.esir.jibambetryx.adapters.MovieAdapter;
+import com.janta.esir.jibambetryx.models.Movie;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by isaiahngaruiya on 17/01/2018.
  */
 
 public class MoviesFragment extends Fragment{
+
+    private RecyclerView recyclerView;
+    private List<Movie> movieList;
+    private MovieAdapter movieAdapter;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater,
                              @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
         View rootView = inflater.inflate(R.layout.movie_fragment, container, false);
-        Bundle args = getArguments();
-        ((TextView)rootView.findViewById(R.id.movie_name)).setText(Integer.toString(args.getInt("Title")));
+        recyclerView = rootView.findViewById(R.id.movies_recycler_view);
+        movieList = new ArrayList<>();
+        movieAdapter = new MovieAdapter(getContext(), movieList);
+        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(getContext(), 2);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(movieAdapter);
+
+        sampleMovies();
         return rootView;
+    }
+
+    private void sampleMovies() {
+        String url = "http://dl.tehmovies.org/94/series/24.legacy/s1/";
+        String [] movies_urls = new String[]{
+                url+"24.Legacy.S01E01.PROPER.480p.Tehmovies_me.mkv",
+                url+"24.Legacy.S01E02.480p.Tehmovies_me.mkv",
+                url+"24.Legacy.S01E03.480p.HDTV.Tehmovies_me.mkv",
+                url+"24.Legacy.S01E04.480p.Tehmovies_me.mkv",
+                url+"24.Legacy.S01E05.480p.Tehmovies_me.mkv"
+        };
+        for(int x=0; x<movies_urls.length; x++) {
+            Movie a = new Movie("Legacy " + (x+1), "https://4.bp.blogspot.com/-jJg6Ohf7Tdw/WJpV583xEvI/AAAAAAAAJKM/rxrNefe-m2UzYcoG-3Ig-e-PxBu7PtrfgCLcB/s1600/16508321_10103551736179324_7216610480964308825_n.jpg", movies_urls[x], "Duration: 2hrs");
+            movieList.add(a);
+        }
+        movieAdapter.notifyDataSetChanged();
     }
 }
---
+++ app/src/main/java/com/janta/esir/jibambetryx/adapters/MovieAdapter.java
@@ -0,0 +1,83 @@
+package com.janta.esir.jibambetryx.adapters;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.janta.esir.jibambetryx.MainActivity;
+import com.janta.esir.jibambetryx.R;
+import com.janta.esir.jibambetryx.models.Movie;
+
+import java.util.List;
+
+/**
+ * Created by isaiahngaruiya on 19/01/2018.
+ */
+
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieViewHolder>{
+
+    private Context mContext;
+    private List<Movie> movieList;
+
+    public MovieAdapter(Context mContext, List<Movie> movieList){
+        this.movieList = movieList;
+        this.mContext = mContext;
+    }
+    @Override
+    public MovieViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View movieView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.movie_card, parent, false);
+        return new MovieViewHolder(movieView);
+    }
+
+    @Override
+    public void onBindViewHolder(MovieViewHolder holder, int position) {
+        //Do the actual inflation of the view
+        final Movie movie = movieList.get(position);
+        holder.movieName.setText(movie.getName());
+        holder.movieDuration.setText(movie.getDuration());
+
+        //For thumbnail(Movie cover) use glide
+        Glide.with(mContext)
+                .load(movie.getThumbnail())
+                .into(holder.thumbnail);
+        holder.thumbnail.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        // Open a new Activity providing the movies data
+                        Intent playerActivity = new Intent(mContext, MainActivity.class);
+                        Bundle args = new Bundle();
+                        args.putString("url", movie.getSource());
+                        playerActivity.putExtras(args);
+                        mContext.startActivity(playerActivity);
+                    }
+                }
+        );
+    }
+
+    @Override
+    public int getItemCount() {
+        return movieList.size();
+    }
+
+    public static class MovieViewHolder extends RecyclerView.ViewHolder{
+
+        public TextView movieName, movieDuration;
+        public ImageView thumbnail;
+
+        public MovieViewHolder(View itemView) {
+            super(itemView);
+            movieName = itemView.findViewById(R.id.movie_name);
+            movieDuration = itemView.findViewById(R.id.movie_duration);
+            thumbnail = itemView.findViewById(R.id.movie_thumbnail);
+        }
+    }
+}
