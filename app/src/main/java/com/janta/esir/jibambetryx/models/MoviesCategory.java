c69066f1ad80e60b82f1299e3f0a4b29b39080d8
esirK <esirkings@gmail.com>
2018-01-21 08:06:24 +0300

    Create a movies category model

---
+++ app/src/main/java/com/janta/esir/jibambetryx/models/MoviesCategory.java
@@ -0,0 +1,31 @@
+package com.janta.esir.jibambetryx.models;
+
+/**
+ * Created by isaiahngaruiya on 21/01/2018.
+ */
+
+public class MoviesCategory {
+    private String name, thumbnail;
+    private int index;
+
+    public MoviesCategory(){
+
+    }
+    public MoviesCategory(String name, int index, String thumbnail){
+        this.name = name;
+        this.index = index;
+        this.thumbnail = thumbnail;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public int getIndex(){
+        return this.index;
+    }
+    public String getThumbnail(){
+        return this.thumbnail;
+    }
+
+}
